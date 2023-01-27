5dd157820850e11a79859491aaf62c45029e88f5
TranPhat-28 <62002249+TranPhat-28@users.noreply.github.com>
2023-01-27 04:09:28 -0500

    ADMIN Delete voucher finished

--- src/components/Admin/VoucherDetail.js
+++ src/components/Admin/VoucherDetail.js
@@ -1,8 +1,11 @@
 import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { AdminAuthContext } from "../../contexts/AdminAuthContext";
 
 const VoucherDetail = (props) => {
 
+    const navigate = useNavigate();
     const { adminUser } = useContext(AdminAuthContext);
     const detailData = props.detailData;
     const detailError = props.detailError;
@@ -29,15 +32,38 @@ const VoucherDetail = (props) => {
 
     const deleteVoucher = (e) => {
         e.preventDefault();
-        
-        if (window.confirm('Do you really want to delete this voucher?')){
-            console.log('delete');
+
+        if (window.confirm('Do you really want to delete this voucher?')) {
+            fetch('/admin/voucher', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `bearer ${adminUser.token}`
+                },
+                body: JSON.stringify({
+                    _id,
+                    voucherCode
+                })
+            })
+                .then(res => {
+                    if (!res.ok) { throw res }
+                    return res.json()
+                })
+                .then(data => {
+                    toast.success(data);
+                    navigate(0);
+                })
+                .catch(e => {
+                    e.json().then(err => {
+                        toast.error(err)
+                    })
+                })
         }
     }
 
     return (
         <div className="mt-4">
-            <h4>Product detail</h4>
+            <h4>Voucher detail</h4>
             {!detailData && <p>Click on a product to view detail</p>}
 
             {detailError && <p>{detailError}</p>}
@@ -45,27 +71,27 @@ const VoucherDetail = (props) => {
             {detailData && <form onSubmit={deleteVoucher}>
                 <label className="form-label mb-0">Voucher ID</label>
                 <input className="form-control mb-2" type="text"
-                    value={_id} onChange={(e) => setid(e.target.value)} disabled></input>
+                    value={_id} disabled></input>
 
                 <label className="form-label mb-0">Voucher code</label>
                 <input className="form-control mb-2" type="text"
-                    value={voucherCode} onChange={(e) => setVoucherCode(e.target.value)} disabled></input>
+                    value={voucherCode} disabled></input>
 
                 <label className="form-label mb-0">Expiration date</label>
                 <input className="form-control mb-2" type="text"
-                    value={exp} onChange={(e) => setExp(e.target.value)} disabled></input>
+                    value={exp} disabled></input>
 
                 <label className="form-label mb-0">Discount percent</label>
                 <input className="form-control mb-2" type="text"
-                    value={percent} onChange={(e) => setPercent(e.target.value)} disabled></input>
+                    value={percent} disabled></input>
 
                 <label className="form-label mb-0">Summary</label>
                 <input className="form-control mb-2" type="text"
-                    value={sum} onChange={(e) => setSum(e.target.value)} disabled></input>
+                    value={sum} disabled></input>
 
                 <label className="form-label mb-0">Description</label>
                 <textarea rows="3" className="form-control mb-2" type="text"
-                    value={desc} onChange={(e) => setDesc(e.target.value)} disabled></textarea>
+                    value={desc} disabled></textarea>
 
                 <button className="btn btn-outline-primary">Delete this voucher</button>
             </form>}
